c126a14d0bce58742449dd98e495dcd0ffcf88f8
Robert Detjens <github@detjens.dev>
2022-03-03 21:48:24 -0800

    more skeleton
    Signed-off-by: Robert Detjens <github@detjens.dev>

--- modules/common.py
+++ modules/common.py
@@ -5,7 +5,7 @@ Common cryptographic helper functions, like XORing byte strings.
 import os
 
 # lambda aka block size
-LAMBDA_BITS = 256  # book uses lambda as # of bits
+LAMBDA_BITS = 128  # book uses lambda as # of bits
 LAMBDA = LAMBDA_BITS / 8  # but bytestrings are in bytes
 
 
@@ -17,10 +17,18 @@ def xor(m: bytes, k: bytes) -> bytes:
 
 
 # salt for hash
-# TODO: should this be done in __main__ etc and passed in instead of a constant?
+# TODO: should this be done in _main_/etc and passed in instead of a constant?
 SALT = os.urandom(LAMBDA)
 
 
 def hash(m: bytes) -> bytes:
-    # do the thing
-    return
+    pass
+
+
+# seed for PRF
+# TODO: should this be done in _main_/etc and passed in instead of a constant?
+SALT = os.urandom(LAMBDA)
+
+
+def prf(s: bytes, x: bytes) -> bytes:
+    pass
--- modules/encrypt.py
+++ modules/encrypt.py
@@ -1,6 +1,17 @@
+import getpass
 import logging
 
 import common
+import keygen
+
+
+def load_key(keyfile):
+    """
+    Decrypt the specified keyfile by asking for the user's password
+    """
+
+    passw = getpass.getpass(prompt="Passphrase for {keyfile}: ")
+    return keygen.unlock_key(passw)  # TODO: replace with actual func
 
 
 def dec(key, infile, outfile):
