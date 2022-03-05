e21557b697e805033f6376faaeecfb23291d9677
lyellread <lyellread@gmail.com>
2022-03-05 15:02:29 -0800

    Adding working key decrypt and checks for decrypted key.
    Modifications to encrypt and decrypt to add documentation inline as
    well as changes to interface with keygen

--- modules/encrypt.py
+++ modules/encrypt.py
@@ -6,24 +6,34 @@ from modules import common, keygen
 def enc(keyfile, infile, outfile):
     logging.debug(f"Encrypting {infile} to {outfile} with keyfile {keyfile}")
 
-    with open(keyfile, "rb") as fkey, open(infile, "rb") as fin, open(outfile, "wb") as fout:
+    with open(infile, "rb") as fin, open(outfile, "wb") as fout:
 
-        key = fkey.read()
+        # Get message from file
         msg = fin.read()
 
+        # Prompt user to decrypt the key
+        key = keygen.decrypt_key(keyfile)
+
+        # Encrypt using the decrypted key
         ctx = common.encrypt(key, msg)
 
+        # Write ciphertext to output file
         fout.write(ctx)
 
 
 def dec(keyfile, infile, outfile):
     logging.debug(f"Decrypting {infile} to {outfile} with keyfile {keyfile}")
 
-    with open(keyfile, "rb") as fkey, open(infile, "rb") as fin, open(outfile, "wb") as fout:
+    with open(infile, "rb") as fin, open(outfile, "wb") as fout:
 
-        key = fkey.read()
-        msg = fin.read()
+        # Get ciphertext from file
+        ctx = fin.read()
 
-        ctx = common.decrypt(key, msg)
+        # Prompt user to decrypt the key
+        key = keygen.decrypt_key(keyfile)
 
-        fout.write(ctx)
+        # Decrypt using the decrypted key
+        msg = common.decrypt(key, ctx)
+
+        # Write the plaintext to the output file.
+        fout.write(msg)
--- modules/keygen.py
+++ modules/keygen.py
@@ -4,7 +4,7 @@ import getpass
 
 
 def keygen(keyfile):
-    logging.debug(f"Creating key '{keyfile}'")
+    logging.debug(f"Creating keyfile '{keyfile}'")
 
     # Check that we are not overwriting a key file
     assert not os.path.exists(keyfile)
@@ -27,3 +27,30 @@ def keygen(keyfile):
     # Store encrypted key to file
     with open(keyfile, "w") as f:
         f.write(encrypted_key.hex())
+
+
+def decrypt_key(keyfile):
+    logging.debug(f"Decrypting and checking keyfile '{keyfile}'")
+
+    # Store encrypted key to file
+    with open(keyfile, "r") as f:
+        encrypted_key = bytes.fromhex(f.read())
+
+    # Decrypt the encrypted key by getting password from the user
+    key_password = getpass.getpass(prompt="Password: ")
+
+    # Calculate hash of user input
+    password_hash = common.hash(key_password.encode())
+
+    # Decrypt and check key
+    key = common.decrypt(password_hash, encrypted_key)
+    if key[common.LAMBDA :] != common.hash(key[: common.LAMBDA]):
+        logging.error("Invalid Password or Corrupted Keyfile. Breaking.")
+        exit()
+
+    else:
+        logging.debug("Key decrypted successfully")
+        key = key[: common.LAMBDA]
+
+    # Return the decrypted key
+    return key
